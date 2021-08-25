67d0a3931540112e3c66be9cd4d5f9d587c4a1ba
Shayan Khorassany <khorassa@ualberta.ca>
2021-08-25 17:57:20 -0600

    QWidget for controling the video feeds

--- main.py
+++ main.py
@@ -1,43 +1,68 @@
-import cv2
-import imutils
-#from imutils import face_utils
-from imutils.video import VideoStream
-#import dlib
-import time
-import datetime
-import numpy as np
-from threading import Thread
-from multiprocessing import Process
+import sys
+from PySide2 import QtCore, QtGui, QtWidgets
+from PySide2.QtCore import QThread, QTimer
+from PySide2.QtGui import QPixmap, QImage
+from PySide2.QtUiTools import QUiLoader
+from PySide2.QtWidgets import QApplication, QMainWindow, \
+    QPushButton, QVBoxLayout, QWidget, QLabel
+# from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+# from matplotlib.figure import Figure
+#from PIL import *
+#from PIL.ImageQt import *
+from PySide2.QtMultimedia import QMediaPlayer
+from camera import Cameras
 
-def showVid(strm, winN):	
-	while True:
-		# grab the frame from the threaded video stream and resize it
-		# to have a maximum width of 400 pixels
-		ret, frame = strm.read()
-		timestamp = datetime.datetime.now()
-		#frame = imutils.resize(frame, width=400)
-		# draw the timestamp on the frame
-		#ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
-		#cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,0.35, (0, 0, 255), 1)
-		# show the frame
-		delt = timestamp - stTime
-		if delt.total_seconds() > 30: 
-			strm.release()
-			cv2.destroyAllWindows()
-			break
-		if ret: cv2.imshow(winN, frame)
-		key = cv2.waitKey(1) & 0xFF
-		# if the `q` key was pressed, break from the loop
-		if key == ord("q"): break
+#matplotlib.use('Qt5Agg') #Render to PySide/PyQt Canvas
 
-webcam1 = cv2.VideoCapture(0)
-webcam2 = cv2.VideoCapture(2)
-webcam1.set(cv2.CAP_PROP_FPS, 10)
-webcam2.set(cv2.CAP_PROP_FPS, 15)
-time.sleep(2.0)
-stTime = datetime.datetime.now()
+class StartWindow(QMainWindow):
+	def __init__(self):
+		super().__init__()
+		self.cams = Cameras()
+		self.cams.init_cam()
+		#ImageQt(self.cams.snapshot_zero().T)
+		self.central_widget = QWidget()
+		self.buttonFrame = QPushButton('Take a photo', self.central_widget)
+		self.buttonFeed = QPushButton('Start feed', self.central_widget)
+		self.fig_scene = QLabel() #Figure(figsize=(640,480), dpi=72, facecolor=(1,1,1), edgecolor=(0,0,0))
+		self.fig_reye = QLabel() #Figure(figsize=(640,480), dpi=72, facecolor=(1,1,1), edgecolor=(0,0,0))
+		self.layout = QVBoxLayout(self.central_widget)
+		self.layout.addWidget(self.buttonFrame)
+		self.layout.addWidget(self.buttonFeed)
+		self.layout.addWidget(self.fig_scene)
+		self.layout.addWidget(self.fig_reye)
+		self.setCentralWidget(self.central_widget)
+		self.update_timer = QTimer()
+		
+		self.update_timer.timeout.connect(self.get_frame)
+		self.buttonFrame.clicked.connect(self.take_photo)
+		self.buttonFeed.clicked.connect(self.start_feed)
+	
+	def show_devs(self):
+		self.new_button = QPushButton(', '.join(str(i) for i in self.cams.list_devices()), self.central_widget)
+		return
+	
+	def take_photo(self):
+		if self.update_timer.isActive(): self.update_timer.stop()
+		self.get_frame()
+	
+	def get_frame(self):
+		im_np = self.cams.pass_defFrame()
+		qimage = QImage(im_np, im_np.shape[1], im_np.shape[0], QImage.Format_RGB888)
+		self.fig_scene.setPixmap(QPixmap(qimage))
+	
+	def start_feed(self):
+		self.update_timer.start()
 
-# loop over the frames from the video stream
-for (stream, winName) in zip((webcam1, webcam2), ("Camera1", "Camera2")):
-	eachCam = Thread(target=showVid, args=(stream, winName,))
-	eachCam.start()
+# class vid_feed(QThread):
+	# def __init__(self, camera):
+		# super().__init__()
+		# self.camera = camera
+	# def run(self):
+		# self.camera.
+
+if __name__ == '__main__':
+	app = QApplication([])
+	window = StartWindow()
+	#window.show_devs()
+	window.show()
+	app.exit(app.exec_())
